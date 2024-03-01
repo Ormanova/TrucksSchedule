c7b9d674b021621849b03e9116a8128c7e0bd23f
VanyaOrm <vanchi_to@abv.bg>
2024-03-01 11:18:53 +0200

    Add project files.

---
+++ TrucksSchedule/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TrucksSchedule.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
