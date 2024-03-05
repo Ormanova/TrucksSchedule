abf485ce313f827e0504758d0d354e0a03351d1e
VanyaOrm <vanchi_to@abv.bg>
2024-03-05 09:41:09 +0200

    Layout, homecontroller and favicon changed

--- TrucksSchedule/Controllers/HomeController.cs
+++ TrucksSchedule/Controllers/HomeController.cs
@@ -18,10 +18,7 @@ namespace TrucksSchedule.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
